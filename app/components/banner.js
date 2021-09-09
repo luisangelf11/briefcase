731233a5ec8c637135a300725d0a644b93731a66
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2021-09-09 19:42:13 -0400

    Change URL me

--- app/components/banner.js
+++ app/components/banner.js
@@ -3,7 +3,7 @@ export function Banner(){
     $banner.classList.add("banner");
     $banner.innerHTML = `
     <div class="box">
-        <img src="app/assets/me.jpg" alt="Luis Angel Fernandez">
+        <img src="./app/assets/me.jpg" alt="Luis Angel Fernandez">
         <h3>Luis Angel Fernandez ~ Desarrollador</h3>
         <p>Si lo puedes imaginar, lo puedes programar</p>
         <a href="https://api.whatsapp.com/send?phone=8292693768" target="_blank" class="call">WhatsApp</a>
