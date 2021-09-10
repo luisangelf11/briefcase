8853cc896835f4cc0274292c0e31b7b442e89e2a
luisangelf11 <71238653+luisangelf11@users.noreply.github.com>
2021-09-09 20:56:35 -0400

    API WhatsApp complete

--- app/components/banner.js
+++ app/components/banner.js
@@ -6,7 +6,7 @@ export function Banner(){
         <img src="https://scontent.fsti4-1.fna.fbcdn.net/v/t1.6435-9/92817662_2689478677964024_3331476500804272128_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=IxOG-WTS-ZwAX-ltK5x&_nc_ht=scontent.fsti4-1.fna&oh=8da0f4696801d59d6e52d093c4528cc3&oe=61615E3F" alt="Luis Angel Fernandez">
         <h3>Luis Angel Fernandez ~ Desarrollador</h3>
         <p>Si lo puedes imaginar, lo puedes programar</p>
-        <a href="https://api.whatsapp.com/send?phone=8292693768" target="_blank" class="call">WhatsApp</a>
+        <a href="https://api.whatsapp.com/send?phone=+1 829-269-3768" target="_blank" class="call">WhatsApp</a>
     </div>
     `;
     return $banner;
